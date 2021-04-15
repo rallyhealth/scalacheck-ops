0d24d08503346c9b465bef7067e4ccf3fa99a0e4
Jeff May <jeff.n.may@gmail.com>
2021-04-14 20:27:12 -0700

    Upgrade to sbt 1.5.0 and switch to dynver / mima
    - Upgrade to Scala 2.13.5 in build files
    - Update build.sbt to support new plugins
    - Remove unused sbt settings
    - Upgrade library dependencies
    - Update Travis CI build to support latest changes
    - Check binary compatibility during CI
    - Ignore sbt.json file

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
 
   final val Scala_2_11 = "2.11.12"
-  final val Scala_2_12 = "2.12.6"
-  final val Scala_2_13 = "2.13.4"
+  final val Scala_2_12 = "2.12.12"
+  final val Scala_2_13 = "2.13.5"
 
   final val ScalaCheck_1_12 = "1.12.6"
   final val ScalaCheck_1_13 = "1.13.5"
@@ -12,10 +12,10 @@ object Dependencies {
   final val ScalaCheck_1_15 = "1.15.2"
 
   private val ScalaTest_2 = "2.2.6"
-  private val ScalaTest_3 = "3.0.8"
+  private val ScalaTest_3 = "3.2.7"
 
-  private val JodaTimeVersion = "2.10"
-  private val TaggingVersion = "2.2.1"
+  private val JodaTimeVersion = "2.10.10"
+  private val TaggingVersion = "2.3.0"
 
   val jodaTime: ModuleID = "joda-time" % "joda-time" % JodaTimeVersion
   val tagging: ModuleID = "com.softwaremill.common" %% "tagging" % TaggingVersion
